72d986bdeaf8080c6475277201b19e01cb1cd319
Naiare7 <naia.arenaza88@gmail.com>
2026-04-22 11:41:34 +0200

    dockerizado y cambiado lo necesario para que funcione

--- src/app.py
+++ src/app.py
@@ -5,20 +5,29 @@ from src.database.db import db
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-# Inicializar la aplicación Flask
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    # Cargar la configuración
+    app.config.from_object(Config)
 
-# Cargar la configuración
-app.config.from_object(Config)
+    # Inicializar extensiones
+    db.init_app(app)
+    Migrate(app, db) 
+    JWTManager(app)
 
-# Inicializar extensiones
-db.init_app(app)
-migrate = Migrate(app, db)
-jwt = JWTManager(app)
+    # Importar modelos (para que Flask y Migrate sepan que existen al arrancar)
+    with app.app_context():
+        from src.models import actores, vehiculos
+        from src.routes.routes import main  # Importar las rutas después de los modelos 
+        
+        if 'routes_main' not in app.blueprints:
+         app.register_blueprint(main)
 
-# Importar modelos (para que Flask y Migrate sepan que existen al arrancar)
-from src.models import actores, vehiculos
+    @app.route('/')
+    def index():
+        return "¡Servidor de Transporte con estructura perfecta funcionando!"
 
-@app.route('/')
-def index():
-    return "¡Servidor de Transporte con estructura perfecta funcionando!"
+
+    return app
+
+# app = create_app()
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,10 +1,10 @@
-from flask import jsonify
-from app import app, Camion
+from flask import Blueprint, jsonify
+from ..database.db import db
+from ..models.vehiculos import Camion
 
-@app.route('/camiones', methods=['GET'])
+main = Blueprint('routes_main', __name__)
+
+@main.route('/camiones', methods=['GET'])
 def get_camiones():
     lista_camiones = Camion.query.all()
     return jsonify([c.to_dict() for c in lista_camiones]), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
